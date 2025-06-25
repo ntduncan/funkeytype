2520b867ac7246ad93b66409318f8b6110d14de8
Nate Duncan <60360613+ntduncan@users.noreply.github.com>
2025-06-25 10:06:40 -0600

    the test

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -17,6 +19,27 @@ import (
 	typetest "ntduncan.com/typer/type-test"
 	"ntduncan.com/typer/utils"
 )
+
+var logFilePath string
+
+func logErrorAndExit(err error) {
+	// This function will be called when a fatal error occurs.
+	// It logs the error to the designated log file and terminates the program.
+	f, fileErr := os.OpenFile(logFilePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if fileErr != nil {
+		// If we can't even open the log file, something is seriously wrong.
+		// We'll print both the original error and the file opening error to stderr.
+		fmt.Fprintf(os.Stderr, "FATAL: could not open log file %s: %v. Original error was: %v\n", logFilePath, fileErr, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// Prepend the timestamp to the log message
+	log.SetOutput(f)
+	log.SetFlags(log.LstdFlags | log.Lshortfile) // Add file/line number
+	log.Output(2, fmt.Sprintf("FATAL: %v", err)) // log the error
+	os.Exit(1)                                   // exit
+}
 
 type Model struct {
 	cursor   int
@@ -35,7 +58,7 @@ func InitModel(width int, height int, size int, mode utils.TestMode) Model {
 
 	modeTopScore, err := Config.GetTopScore()
 	if err != nil {
-		panic(err)
+		logErrorAndExit(err)
 	}
 	BestWPM = modeTopScore
 
@@ -67,7 +90,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 					TopScores: Config.TopScores,
 				}
 				if err := system.SaveConfig(config); err != nil {
-					panic(fmt.Sprintf("There was an error saving your configuration: %s", err))
+					logErrorAndExit(fmt.Errorf("there was an error saving your configuration: %w", err))
 				}
 
 				return m, tea.Quit
@@ -126,7 +149,7 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 						TopScores: Config.TopScores,
 					}
 					if err := system.SaveConfig(config); err != nil {
-						panic(fmt.Sprintf("There was an error saving your configuration: %s", err))
+						logErrorAndExit(fmt.Errorf("there was an error saving your configuration: %w", err))
 					}
 
 					return m, tea.Quit
@@ -303,9 +326,28 @@ func (m Model) footer() string {
 }
 
 func main() {
+	// Setup logging file path and directory at the start.
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "critical: unable to find user home directory: %v\n", err)
+		os.Exit(1)
+	}
+	logDir := filepath.Join(homeDir, ".config", "funkeytype", "logs")
+
+	// Create the log directory if it doesn't exist.
+	// 0755 gives rwx for owner, rx for group and others.
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		fmt.Fprintf(os.Stderr, "critical: unable to create log directory %s: %v\n", logDir, err)
+		os.Exit(1)
+	}
+
+	// Format the log file name with a timestamp.
+	logFileName := "funkeytype-log-" + time.Now().Format("2006-01-02_15-04-05") + ".log"
+	logFilePath = filepath.Join(logDir, logFileName)
+
 	config, configErr := system.LoadConfig()
 	if configErr != nil {
-		panic(configErr)
+		logErrorAndExit(configErr)
 	}
 
 	Config = config
@@ -316,3 +358,4 @@ func main() {
 		os.Exit(1)
 	}
 }
+
